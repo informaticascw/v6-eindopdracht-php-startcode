050d0f3a8d422f9a923789c64f9cfe12fccdb377
jcoonrod <54367582+jcoonrod@users.noreply.github.com>
2021-05-26 14:35:20 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head><title>My App</title>
+<link rel="icon" type="image/svg+xml" href="/favicon.svg">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name = "Description" content = "My first database app">
+<style> * {font-family:sans-serif;}
+nav, th {color:white; background-color:navy;padding:5px;}
+nav a {color:white; font-weight:bold; text-decoration:none;}
+nav a:hover {background-color:green;}
+</style>
+</head>
+<body>
+<nav>
+<a href="/">Home</a> |
+<a href="/people.php">People</a> |
+<a href="/products.php">Products</a> |
+<a href="/phpliteadmin.php" target="_blank">Admin in new tab</a>
+</nav>
